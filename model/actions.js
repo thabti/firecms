4bd87fb1e1fc682f42e78d34fa85589ed1a29c69
Sabeur Thabti <thabti.sabeur@gmail.com>
2015-12-18 09:20:04 +0000

    introduce firebase

--- model/actions.js
+++ model/actions.js
@@ -1,7 +1,20 @@
 export const GET_DATA = 'GET_DATA';
+var Firebase = require('firebase')
+var myFirebaseRef = new Firebase("https://stylecouncil.firebaseio.com/");
 export function getData(links = {'data': 'hello'}) {
   return {
     type: GET_DATA,
     links
   };
+}
+
+export function saveData(data) {
+ return function thunk(dispatch) {
+    myFirebaseRef.push(data, function(error) {
+         if (error) { console.log(`Error: ${error}`)} else { dispatch(getData(data)) }
+       });
+  }
+
+
+
 }
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../model/reducers.js';
-import {getData, GET_DATA} from '../model/actions.js';
+import {getData, GET_DATA, saveData} from '../model/actions.js';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
 module.exports = function (app) {
-  console.log(store)
+
   app.get('/', function (req, res) {
     var isDev = process.env.NODE_ENV !== 'production'
 
-
-
+    store.subscribe(() =>
+      console.log(store.getState())
+    )
     var data = store.getState().getData.links;
     res.render('main', {
       year: JSON.stringify(data.year)
@@ -21,8 +22,12 @@ module.exports = function (app) {
   app.get('/abc', function (req, res) {
     var q = req.query.year || '2016';
     console.log(q)
-    store.dispatch(getData({"year": q }));
+    store.dispatch(saveData({"year": q }));
     res.end("done", store)
   });
+
+  // app.get('/post', (req, res) {
+  //
+  // });
 
 };
